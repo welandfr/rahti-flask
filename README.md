8ee3ee4da07c88ca736466d54f3151589b862524
Fredrik Welander <fredrik.welander@subsite.fi>
2022-01-14 21:47:00 +0200

    Test smtplib

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # rahti-flask
 
-- Add webhook
+- Create python-project on CSC Rahti
+- Copy-paste the GitHub Webhook URL to GitHub/Settings/Webhooks/New Webhook. 
+- Set Webhook Content type to application/json
+
+
+## POST /sendmail
+```
+{ 
+    "to": "user@server.domain", 
+    "subject": "Subject", 
+    "body": "Body" 
+}
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, jsonify
+import os, smtplib
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +8,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    ret = {'messgage': 'Hello', 'smtp': os.environ.get('MAIL_SMTP')}
+    ret = {'message': 'This is the email service.' }
 
-    return jsonify(ret)
+    return ret
+
+
+@app.route("/sendmail", methods = [ 'POST' ])
+def send():
+    ret = { 'smtp': os.environ.get('MAIL_SMTP')}
+
+    req = request.get_json()
+
+    msg = f"""From: {os.environ.get('MAIL_FROM')}
+        To: {req['to']}
+        Subject: {req['subject']}
+
+        {req['body']}"""
+
+    try:
+        smtp_obj = smtplib.SMTP(os.environ.get('MAIL_SMTP'))
+        smtp_obj.sendmail(req['from'], [req['to']], msg)
+
+        ret = { 'message': 'Mail sent' }
+
+    except Exception as e:
+        ret =  { 'error': 'Mail fail.' }
+        print(e)
+    
+    return ret
 
 if __name__ == "__main__":
     app.run(debug=True,
